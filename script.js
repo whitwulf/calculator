152b018afa4277be389faaf2b5650ea1c53f7fab
whitwulf <whitwulf@gmail.com>
2022-08-05 23:11:46 -0500

    Style initial calculator layout

---
+++ script.js
@@ -0,0 +1,25 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const operate = (a, op, b) => {
+  switch (op) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+    default:
+      console.log(`Sorry didn't understand`);
+  }
+};
